bd12c244081e60e1688b9130c22aa3d92855e0fe
Ahmad <133608594+ahmad458953@users.noreply.github.com>
2025-06-13 14:15:17 +0300

    Update hash_cracker.py

--- hash_cracker.py
+++ hash_cracker.py
@@ -1,15 +1,17 @@
 import hashlib
-hash = input("enter your hash ")
+
+hash_input = input("enter your hash ")
 passls = input("pls enter pass list ")
+
 try:
-	with open(passls, "r") as f:
-		for guess in f:
-			guess = guess.strip()
-			hashed = hashlib.md5(guess.encode()).hexdigest()
-			if hashed == hash:
-				print(f"[+] password found: {guess}")
-				break
-			else:
-				print(f"[/] password not found")
+    with open(passls, "r") as f:
+        for guess in f:
+            guess = guess.strip()
+            hashed = hashlib.md5(guess.encode()).hexdigest()
+            if hashed == hash_input:
+                print(f"[+] password found: {guess}")
+                break
+        else:
+            print("[/] password not found")
 except FileNotFoundError:
-	print(f"[-] pass list file not found")
+    print("[-] pass list file not found")

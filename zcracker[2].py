78de4e022530414da58ad6b6290e5449156a2b7b
Ahmad <133608594+ahmad458953@users.noreply.github.com>
2025-06-13 13:52:39 +0300

    Update zcracker[2].py

--- zcracker[2].py
+++ zcracker[2].py
@@ -4,12 +4,12 @@ def is_passwd_correct(password, zip_path):
     with zipfile.ZipFile(zip_path) as zf:
         try:
             for name in zf.namelist():
-                zf.read(name, pwd=password.encode())
+                zf.read(name, pwd=str(password).encode())
             return True
         except:
             return False
 
-zip_path = input("enter zip file path")
+zip_path = input("Enter zip file path: ")
 
 for password in range(10000):
     if is_passwd_correct(password, zip_path):
